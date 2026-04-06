b0fefa50afa5f4c674783c7f3ff6050459fb2be3
Dev <dev@Devs-MacBook-Pro.local>
2026-04-06 20:04:07 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.danendz.marquer',
+  appName: 'marquer-mobile-web',
+  webDir: 'dist'
+};
+
+export default config;
